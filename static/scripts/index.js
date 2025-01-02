58ac837455766f7565886c7d1d5d7bcabea44fc8
FA <fisayoaasa@gmail.com>
2025-01-02 23:09:36 +0100

    added more features to the website

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,7 +1,24 @@
-var navigationOptions = document.querySelectorAll(".navigation-page a");
-document.addEventListener('DOMContentLoaded', function() {
-    var currentPage = window.location.pathname;
+var navigationOptions = $(".navigation-page a");
+var editButton = $(".edit");
 
+$(document).ready(function() {
+    highlightCurrentPage();
+
+    editButton.on("click", function() {
+        $(".edit-blog").removeClass("hidden");
+        $("nav, .posts").addClass("blur");
+    });
+
+    $(".cancel").on("click", function() {
+        $(".edit-blog").addClass("hidden");
+        $("nav, .posts").removeClass("blur");
+    });
+    
+});
+
+function highlightCurrentPage(){
+    var currentPage = window.location.pathname;
+    
     if (currentPage === '/templates/about.html') {
         navigationOptions[3].classList.add("bold");
     } else if (currentPage === '/templates/contact.html') {
@@ -9,4 +26,4 @@ document.addEventListener('DOMContentLoaded', function() {
     } else if (currentPage === '/templates/post.html') {
         navigationOptions[1].classList.add("bold");
     }
-});
+}
