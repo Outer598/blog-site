f4e1d8694fe809afcf955420abbd64a333813cf5
FA <fisayoaasa@gmail.com>
2025-01-03 20:18:57 +0100

    Completed the blog post page

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -9,9 +9,56 @@ $(document).ready(function() {
         $("nav, .posts").addClass("blur");
     });
 
+    $(".write-new").on("click", function() {
+        $(".new-blog").removeClass("hidden");
+        $("nav, .posts").addClass("blur");
+    });
+
+    $(".save").on("click", function(event) {
+        event.preventDefault();
+        $(".edit-blog").addClass("hidden");
+        $("nav, .posts").removeClass("blur");
+        $(".status").css("background-color", "green");
+        $(".status h2").html("Updated successfully");
+        $(".status").fadeIn(2000).fadeOut(2000);
+    });
+
+    $(".add").on("click", function(event) {
+        event.preventDefault();
+        $(".new-blog").addClass("hidden");
+        $("nav, .posts").removeClass("blur");
+        $(".status").css("background-color", "green");
+        $(".status h2").html("Added successfully");
+        $(".status").fadeIn(2000).fadeOut(2000);
+    });
+
     $(".cancel").on("click", function() {
         $(".edit-blog").addClass("hidden");
         $("nav, .posts").removeClass("blur");
+    });
+
+    
+    $(".cancel").on("click", function() {
+        $(".new-blog").addClass("hidden");
+        $("nav, .posts").removeClass("blur");
+    });
+
+    $(".delete").on("click", function() {
+        $(".delete-post").removeClass("hidden");
+        $("nav, .posts").addClass("blur");
+    });
+
+    $(".delete-post .yes").on("click", function() {
+        $(".delete-post").addClass("hidden");
+        $("nav, .posts").removeClass("blur");
+        $(".status h2").html("Deleted successfully");
+        $(".status").css("background-color", "red");
+        $(".status").fadeIn(2000).fadeOut(2000);
+    });
+
+    $(".delete-post .no").on("click", function() {
+        $(".delete-post").addClass("hidden");
+        $("nav, .posts").removeClass("blur");   
     });
     
 });
